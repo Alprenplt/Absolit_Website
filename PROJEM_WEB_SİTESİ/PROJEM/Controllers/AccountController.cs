af96ad3072cbcb68921c1d602c56f92dd5d5c2e7
Alperen POLAT <104601012+Alprenplt@users.noreply.github.com>
2022-11-16 00:42:55 +0300

    Absolit Doğal Taşlar Website
    A website with information about natural stones..

---
+++ PROJEM_WEB_SİTESİ/PROJEM/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using PROJEM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace PROJEM.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+       
+        Takip_SistemiEntities db=new Takip_SistemiEntities();   
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+        [HttpPost]
+        public ActionResult Login(Kullanici p)
+        {
+            var bilgiler=db.Kullanici.FirstOrDefault(x=>x.KullaniciAd==p.KullaniciAd && x.Sifre==p.Sifre); 
+            if (bilgiler!=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.KullaniciAd, false);
+                Session["Ad"]=bilgiler.Ad.ToString();
+                Session["Soyad"] = bilgiler.Soyad.ToString();
+                return RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                ViewBag.hata = "Kullanıcı Adı Veya Şifre Hatalı";
+            }
+
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Kullanici a)
+        {
+            db.Kullanici.Add(a);
+            a.Rol = "U";
+            db.SaveChanges();
+            return RedirectToAction("Login", "Account");
+
+        }
+
+
+    }
+
+}
---
+++ PROJEM_WEB_SİTESİ/PROJEM/Controllers/HomeController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PROJEM.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DogalTaslar()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Burclar()
+        {
+
+
+            return View();
+        }
+        public ActionResult GırısYap()
+        {
+
+
+            return View();
+        }
+        public ActionResult UyeOl()
+        {
+
+
+            return View();
+        }
+        public ActionResult Iletısım()
+        {
+
+
+            return View();
+        }
+        public ActionResult Spinel()
+        {
+
+
+            return View();
+        }
+        public ActionResult Akik()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Akuamarin()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Barit()
+        {
+
+
+            return View();
+        }
+
+
+        public ActionResult Gunestası()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult KaplanGözü()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult İyolit()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Mercan()
+        {
+
+
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
